698e6af96d24a412bc4b9416b1748fa39350d1c1
Yuki Kishimoto <yukikishimoto@protonmail.com>
2024-11-03 13:08:09 +0100

    Remove `future-util` from `prelude` module

--- src/prelude.rs
+++ src/prelude.rs
@@ -7,9 +7,6 @@
 #![allow(ambiguous_glob_reexports)]
 #![doc(hidden)]
 
-// External crates
-pub use futures_util::*;
-
 // Internal modules
 #[cfg(all(feature = "tor", not(target_arch = "wasm32")))]
 pub use crate::native::tor::*;
